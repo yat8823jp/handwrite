40a2d3bca47b7f821c4fc86a5bcf312befdac5be
y_kawanishi <yat@yat-net.com>
2024-06-03 22:43:14 +0900

    touch-event-fix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const drawCanvas     = document.getElementById( "drawCanvas" );
 const drawTempCanvas = document.getElementById( "drawTempCanvas" );
 const pointerCanvas  = document.getElementById( "pointerCanvas" );
 
+const FRAMERATE = 60;
 let timer = 0;
 
 const ctxs = {
@@ -19,77 +20,11 @@ let option = {
 	startX     : 0,
 	startY     : 0
 }
-
-if ( imageCanvas.getContext && drawCanvas.getContext && drawTempCanvas.getContext && pointerCanvas.getContext ) {
-
-	window.addEventListener( 'load', function ( e ) {
-
-		pointerCanvas.addEventListener( 'mousedown', mouseDown );
-		pointerCanvas.addEventListener( 'mousemove', mouseMove );
-		pointerCanvas.addEventListener( 'mouseup',   mouseUp );
-
-		pointerCanvas.addEventListener( 'mouseout', function ( e ) {
-			ctxs.pointerCtx.clearRect( 0, 0, imageCanvas.width, imageCanvas.height );
-			if ( option.holdClick ) mouseUp( e );
-		} );
-		zoom();
-	} );
-	window.addEventListener( 'resize', zoom() );
-}
-
-function mouseDown( e ) {
-	option.holdClick = true;
-	option.startX = e.offsetX;
-	option.startY = e.offsetY;
-}
-
-function mouseMove( e ) {
-	pointer( e );
-	if ( option.holdClick ) {
-		drawPen( e );
-	}
-}
-
-function mouseUp( e ) {
-	option.holdClick = false;
-	drawPen( e );
-}
-
-function drawPen( e ) {
-	ctxs.drawCtx.lineWidth                = option.brushSize;
-	ctxs.drawCtx.strokeStyle              = option.strokeColor;
-	ctxs.drawCtx.lineJoin                 = "round";
-	ctxs.drawCtx.lineCap                  = "round";
-	ctxs.drawCtx.beginPath();
-	ctxs.drawCtx.moveTo( option.startX, option.startY );
-	ctxs.drawCtx.lineTo( e.offsetX, e.offsetY );
-	ctxs.drawCtx.stroke();
-	ctxs.drawCtx.closePath();
-	option.startX                         = e.offsetX;
-	option.startY                         = e.offsetY;
-}
-
-function pointer( e ) {
-	ctxs.pointerCtx.clearRect( 0, 0, imageCanvas.width, imageCanvas.height );
-
-	if ( option.holdClick ) {
-		drawPen( e );
-	}
-	ctxs.pointerCtx.beginPath();
-	ctxs.pointerCtx.moveTo( e.offsetX, e.offsetY );
-	ctxs.pointerCtx.lineTo( e.offsetX, e.offsetY );
-	ctxs.pointerCtx.stroke();
-	ctxs.pointerCtx.closePath();
-}
-
-function zoom() {
+let zoom = () => {
 	let displayWidth  = drawCanvas.clientWidth;
 	let displayHeight = drawCanvas.clientHeight;
-  	// canvasの「描画バッファーのサイズ」と「表示サイズ」が異なるかどうか確認する。
+  	// Check to Buffer size and display size for responsive.
 	if ( drawCanvas.width != displayWidth || drawCanvas.height != displayHeight ) {
-
-		// サイズが違っていたら、描画バッファーのサイズを
-		// 表示サイズと同じサイズに合わせる。
 		imageCanvas.width     = displayWidth;
 		imageCanvas.height    = displayHeight;
 		drawCanvas.width      = displayWidth;
@@ -100,3 +35,89 @@ function zoom() {
 		pointerCanvas.height  = displayHeight;
 	}
 }
+
+if ( imageCanvas.getContext && drawCanvas.getContext && drawTempCanvas.getContext && pointerCanvas.getContext ) {
+
+	window.addEventListener( 'load', function ( e ) {
+		pointerCanvas.addEventListener( 'mousedown',  mouseDown );
+		pointerCanvas.addEventListener( 'touchstart', mouseDown );
+		pointerCanvas.addEventListener( 'mousemove',  mouseMove );
+		pointerCanvas.addEventListener( 'touchmove',  mouseMove );
+		pointerCanvas.addEventListener( 'mouseup',    mouseUp );
+		pointerCanvas.addEventListener( 'touchend',   function( e ) { e.preventDefault(); mouseUp } );
+
+		pointerCanvas.addEventListener( 'mouseout', function ( e ) {
+			ctxs.pointerCtx.clearRect( 0, 0, imageCanvas.width, imageCanvas.height );
+			if ( option.holdClick ) mouseUp( e );
+		} );
+		zoom();
+	} );
+	window.addEventListener( 'resize', zoom() );
+}
+
+let mouseDown = ( e ) => {
+	let offsetX, offsetY;
+	let offsets = getOffsets( e );
+	option.holdClick = true;
+	option.startX = offsets.x;
+	option.startY = offsets.y;
+	return option;
+}
+
+let getOffsets = ( e ) => {
+	const rect = e.target.getBoundingClientRect();
+	let offsets = {
+		x: 0,
+		y: 0
+	};
+	if ( e.touches ) {
+		offsets.x = Math.floor( e.touches[0].clientX - rect.left );
+		offsets.y = Math.floor( e.touches[0].clientY - rect.top );
+	} else {
+		offsets.x = e.offsetX;
+		offsets.y = e.offsetY;
+	}
+	return offsets;
+}
+
+let mouseMove = ( e ) => {
+	pointer( e );
+	if ( option.holdClick ) {
+		e.preventDefault();
+		drawPen( e );
+	}
+}
+
+let mouseUp = ( e ) => {
+	option.holdClick = false;
+	drawPen( e );
+}
+
+let drawPen = ( e ) => {
+	let offsets = getOffsets( e );
+	ctxs.drawCtx.lineWidth                = option.brushSize;
+	ctxs.drawCtx.strokeStyle              = option.strokeColor;
+	ctxs.drawCtx.lineJoin                 = "round";
+	ctxs.drawCtx.lineCap                  = "round";
+	ctxs.drawCtx.beginPath();
+	ctxs.drawCtx.moveTo( option.startX, option.startY );
+	ctxs.drawCtx.lineTo( offsets.x, offsets.y );
+	ctxs.drawCtx.stroke();
+	ctxs.drawCtx.closePath();
+	option.startX = offsets.x;
+	option.startY = offsets.y;
+}
+
+let pointer = ( e ) => {
+	let offsets = getOffsets( e );
+	ctxs.pointerCtx.clearRect( 0, 0, imageCanvas.width, imageCanvas.height );
+	if ( option.holdClick ) {
+		drawPen( e );
+	}
+	ctxs.pointerCtx.beginPath();
+	ctxs.pointerCtx.moveTo( offsets.x, offsets.y );
+	ctxs.pointerCtx.lineTo( offsets.x, offsets.y );
+	ctxs.pointerCtx.stroke();
+	ctxs.pointerCtx.closePath();
+}
+
